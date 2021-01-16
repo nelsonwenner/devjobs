15c55182f967f04f4731b510595a4001a5bc8545
wenner <nelsondiasdemedeiros@gmail.com>
2021-01-16 15:51:48 -0300

    chore: add dockerize

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+cd /home/rails/app
+
 bundle install
+
+dockerize -wait tcp://postgres:5432 -timeout 2700s -wait-retry-interval 10s
+
 rails db:create
 rails db:migrate
 rails server -p 3333 -b 0.0.0.0
