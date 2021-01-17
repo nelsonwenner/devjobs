3fe0594859c56e052b733aa574f3eb70c0538d02
wenner <nelsondiasdemedeiros@gmail.com>
2021-01-17 15:40:20 -0300

    refactor: refat scripts

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cd /home/rails/app
-
-bundle install
-yarn install
+cd /usr/src/app
 
 dockerize -wait tcp://postgres:5432 -timeout 2700s -wait-retry-interval 10s
 
-rails db:create
-rails db:migrate
-rails server -p 3333 -b 0.0.0.0
+(bundle check || bundle install)
+yarn install
+
+bundle exec rails db:create
+bundle exec rails db:migrate
+bundle exec rails server -p 3333 -b '0.0.0.0'
