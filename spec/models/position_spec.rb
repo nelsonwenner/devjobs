ed1cba13df9bce32f081f463843467dd1e8f3387
wenner <nelsondiasdemedeiros@gmail.com>
2021-01-19 20:05:04 -0300

    test: add test in model associations

--- spec/models/position_spec.rb
+++ spec/models/position_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Position, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associations' do
+    it { should belong_to(:company) }
+    it { should have_many(:applicant) }
+  end
 end
