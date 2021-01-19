6185a6ffb22acc31963ea37d8a23d448cf6e1ba4
wenner <nelsondiasdemedeiros@gmail.com>
2021-01-18 22:43:58 -0300

    feat: add methods tokens and hash password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,42 @@ class User < ApplicationRecord
   validates_presence_of :username, :email, :password on: :create
   validates_uniqueness_of :email
   
+  # Generate a new password reset token for a user.
+  #
+  # @return [Nil]
+  def generate_password_token!
+    self.update_columns(
+      reset_password_token: generate_token,
+      reset_password_sent_at: Time.current
+    )
+  end
+
+  # Check if password token is valid.
+  #
+  # @return [Boolean]
+  def password_token_valid?
+    expires_at = self.reset_password_sent_at + 1.hour
+    expires_at > Time.current
+  end
+
+  # Reset password.
+  #
+  # @param password [String]
+  def reset_password!(password)
+    self.update_columns(
+      reset_password_token: nil,
+      password: password
+    )
+  end
+
+  private
+
+  # Generate a password token.
+  # Uses SecureRandom#hex.
+  #
+  # @return [String]
+  def generate_token
+    SecureRandom.hex
+  end
+  
 end
