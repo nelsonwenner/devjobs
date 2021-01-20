0a5489bd957478679969e0020f47f75ba1428492
wenner <nelsondiasdemedeiros@gmail.com>
2021-01-20 11:41:52 -0300

    test: added tests of validations

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Company, type: :model do
     it { should belong_to(:user) }
     it { should have_many(:position) }
   end
+
+  describe 'Validations' do
+    it { should validate_presence_of(:name).on(:create) }
+    it { should validate_presence_of(:url).on(:create) }
+  end
 end
