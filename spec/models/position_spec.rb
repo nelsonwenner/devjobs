bd806f88d16be5579b00e25d43a29ca4eb13145d
wenner <nelsondiasdemedeiros@gmail.com>
2021-01-20 12:35:58 -0300

    test: added tests of validations

--- spec/models/position_spec.rb
+++ spec/models/position_spec.rb
@@ -5,4 +5,17 @@ RSpec.describe Position, type: :model do
     it { should belong_to(:company) }
     it { should have_many(:applicant) }
   end
+
+  describe 'Validations' do
+    it { should validate_presence_of(:name).on(:create) }
+    it { should validate_presence_of(:career).on(:create) }
+    it { should validate_presence_of(:contract).on(:create) }
+    it { should validate_presence_of(:remote).on(:create) }
+    it { should validate_presence_of(:country).on(:create) }
+    it { should validate_presence_of(:state).on(:create) }
+    it { should validate_presence_of(:city).on(:create) }
+    it { should validate_presence_of(:summary).on(:create) }
+    it { should validate_presence_of(:description).on(:create) }
+    it { should validate_presence_of(:publish).on(:create) }
+  end
 end
