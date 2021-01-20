a38bd8c413ba6ccfdf30c45ae3d42214f083ecd6
wenner <nelsondiasdemedeiros@gmail.com>
2021-01-20 11:44:27 -0300

    feat: add validations

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,4 +1,6 @@
 class Position < ApplicationRecord
   belongs_to :company
   has_many :applicant, dependent: :destroy
+
+  validates_presence_of :name, :career, :contract, :remote, :country, :state, :city, :summary, :description, :publish, on: :create
 end
