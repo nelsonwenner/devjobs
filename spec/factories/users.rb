6e3e5d1de084ffd0189e4d43817cc6faf1fb8eda
wenner <nelsondiasdemedeiros@gmail.com>
2021-01-20 20:03:12 -0300

    refactor: add password in mock

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
-  factory :user do
-    
+  factory :user, class: 'User' do
+    username { Faker::Name.name }
+    email { Faker::Internet.email }
+    password { '123456' }
   end
 end
