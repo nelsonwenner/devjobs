a7bcd35b5554b708f270f0f105b89c3863dde116
wenner <nelsondiasdemedeiros@gmail.com>
2021-01-20 20:19:00 -0300

    feat: add method not_found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
   protect_from_forgery with: :exception
+  
+  def not_found(exception)
+    render status: 404, json: {errors: exception.model + ' not found' }.to_json
+  end
 end
