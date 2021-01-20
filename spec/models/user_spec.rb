602caacd0ce14435cb0570b80a040019411db1e3
wenner <nelsondiasdemedeiros@gmail.com>
2021-01-20 11:25:14 -0300

    test: added tests of validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe User, type: :model do
     it { should have_one(:company) }
     it { should have_many(:applicant) }
   end
+  
+  describe 'Validations' do
+    it { should validate_presence_of(:username).on(:create) }
+    it { should validate_presence_of(:email).on(:create) }
+    it { should validate_presence_of(:password).on(:create) }
+  end
 end
