0454f4750eda6b2cb2b66b529d9dc795db4f75d7
wenner <nelsondiasdemedeiros@gmail.com>
2021-01-20 15:36:22 -0300

    chore: add comment

--- app/controllers/concerns/authable.rb
+++ app/controllers/concerns/authable.rb
@@ -5,12 +5,17 @@ module Authable
     before_action :current_user
   end
 
+  # if not exists a session with this user-id, he returns nil.
+  # If exists, search user through user-id stored a session,
+  # and store in the global variable @current_user.
+  #
   def current_user
-    return nil unless session[:user_id]
-    
+    return nil unless session[:user_id]  
     @current_user ||= User.find(session[:user_id])
   end
   
+  # If not exists a current_user.
+  #
   def authenticate
     render json: { error: 'Access Denied' }, status: 401 unless current_user
   end
