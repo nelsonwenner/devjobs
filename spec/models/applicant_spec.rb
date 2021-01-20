6ed30535651146ece69e4ae8cfd147252e290f10
wenner <nelsondiasdemedeiros@gmail.com>
2021-01-20 12:39:33 -0300

    test: added tests of validations

--- spec/models/applicant_spec.rb
+++ spec/models/applicant_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe Applicant, type: :model do
     it { should belong_to(:user) }
     it { should belong_to(:position) }
   end
+
+  describe 'Validations' do
+    it { should validate_presence_of(:name).on(:create) }
+    it { should validate_presence_of(:email).on(:create) }
+    it { should validate_presence_of(:phone).on(:create) }
+  end
 end
