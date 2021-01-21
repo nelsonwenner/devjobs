3c2da0b0ecd30063009dac1987745158ab753d31
wenner <nelsondiasdemedeiros@gmail.com>
2021-01-21 14:44:08 -0300

    refactor: add a dictionary of a use in the params of the test

--- spec/controllers/auths_controller_spec.rb
+++ spec/controllers/auths_controller_spec.rb
@@ -6,11 +6,13 @@ RSpec.describe ::Api::V1::AuthsController, type: :controller do
 
   context 'Authentication' do
     let(:user) { create(:user) }
-
+    
     it 'Should accomplish login return a JSON with status success and logged_in true' do
       response = post :create, params: { 
-        email: user.email, 
-        password: '123456' 
+        user: {
+          email: user.email, 
+          password: '123456'
+        } 
       }
       expect(eval(response.body)).to eq({ 
         status: 'success', logged_in: true 
@@ -19,8 +21,10 @@ RSpec.describe ::Api::V1::AuthsController, type: :controller do
 
     it 'Should not accomplish login return a JSON with status error and logged_in false' do
       response = post :create, params: { 
-        email: user.email, 
-        password: '12345678' 
+        user: {
+          email: user.email, 
+          password: '12345678'
+        } 
       }
       expect(eval(response.body)).to eq({ 
         status: 'error', logged_in: false 
@@ -30,8 +34,10 @@ RSpec.describe ::Api::V1::AuthsController, type: :controller do
     it 'Should verify it is connected return a JSON with current user logged_in true' do
       session[:user_id] = user.id
       response = get :logged_in, params: { 
-        email: user.email, 
-        password: '123456' 
+        user: {
+          email: user.email, 
+          password: '123456'
+        } 
       }
       expect(eval(response.body)).to eq({ 
         username: user.username, 
@@ -43,8 +49,10 @@ RSpec.describe ::Api::V1::AuthsController, type: :controller do
     it 'Should accomplish logout return a json with and logged_in false' do
       session[:user_id] = user.id
       response = delete :logout, params: { 
-        email: user.email, 
-        password: '123456' 
+        user: {
+          email: user.email, 
+          password: '123456'
+        } 
       }
       expect(eval(response.body)).to eq({ logged_in: false })
     end
