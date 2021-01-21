2d0df375477d6fab219c9d6a0a7a529126199e2e
wenner <nelsondiasdemedeiros@gmail.com>
2021-01-21 12:04:40 -0300

    test: ensure the return of a JSON with status success, and logged-in true

--- spec/controllers/auths_controller_spec.rb
+++ spec/controllers/auths_controller_spec.rb
@@ -3,4 +3,18 @@ require 'rails_helper'
 RSpec.describe ::Api::V1::AuthsController, type: :controller do
   # Shared concern Authable 
   it_behaves_like 'Authable', ::Api::V1::AuthsController
+
+  context 'Authentication' do
+    let(:user) { create(:user) }
+    
+    it 'Should return a json with status sucess and logged_in true' do
+      response = post :create, params: { 
+        email: user.email, 
+        password: '123456' 
+      }
+      expect(eval(response.body)).to eq({ 
+        status: 'success', logged_in: true 
+      })
+    end
+  end
 end
