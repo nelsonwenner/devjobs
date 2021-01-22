e2bbd5f7e3f981cc0192cd6102ba959d0785d4cf
wenner <nelsondiasdemedeiros@gmail.com>
2021-01-22 12:02:44 -0300

    feat: add route registration

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   
   namespace :api do
     namespace :v1 do
+      resources :registrations, only: [:create]    
       resources :auths, only: [:create] do
         collection do
           get 'me', to: 'auths#logged_in'
