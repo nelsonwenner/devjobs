0edf0ad1cf9a6c3e6d1b1fb1fa0463813109b616
wenner <nelsondiasdemedeiros@gmail.com>
2021-01-22 13:01:40 -0300

    refactor: add a semantics of test

--- spec/concerns/authable_spec.rb
+++ spec/concerns/authable_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
 RSpec.shared_examples 'Authable' do
-  it "should return true if user is logged in" do
-    session[:user_id] = 1
-    expect(subject.logged_in?).to eq(true)
-  end
-  
-  it "should return false if user is not logged in" do
-    expect(subject.logged_in?).to eq(false)
+  describe '#logged_in' do
+    context 'When logged in' do
+      it {
+        session[:user_id] = 1
+        expect(subject.logged_in?).to eq(true)
+      }
+    end
+    
+    context "When logged out" do
+      it {
+        expect(subject.logged_in?).to eq(false)
+      }
+    end
   end
 end
