186b6a3087f2dcb5eb90e8f2c06f1ae2187c43e8
wenner <nelsondiasdemedeiros@gmail.com>
2021-01-22 10:45:41 -0300

    feat: add slugify

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,5 +2,11 @@ class Company < ApplicationRecord
   belongs_to :user
   has_many :position, dependent: :destroy
   
+  before_create :slugify
+  
+  def slugify
+    self.slug = name.parameterize
+  end
+
   validates_presence_of :name, :url, on: :create
 end
