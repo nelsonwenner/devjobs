3c1a4ec63faee7f7396a69033bcd4281d0d417ec
wenner <nelsondiasdemedeiros@gmail.com>
2021-01-23 21:32:45 -0300

    test: add a company factory

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -1,8 +1,13 @@
 FactoryBot.define do
   factory :company do
-    name { "MyString" }
-    url { "MyString" }
-    brand { "MyString" }
-    user { nil }
+    name { Faker::Company.name }
+    url { Faker::Internet.url(host: 'example.com') }
+    brand { 
+      Faker::Avatar.image(
+        slug: "my-own-slug", 
+        size: "50x50", 
+        format: "jpg"
+      ) 
+    }
   end
 end
