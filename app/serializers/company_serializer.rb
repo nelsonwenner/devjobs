527f641b59e6fdc206ffd0842cff9fea4d2ca2d4
wenner <nelsondiasdemedeiros@gmail.com>
2021-01-24 13:51:31 -0300

    feat: add a URL in the brand

--- app/serializers/company_serializer.rb
+++ app/serializers/company_serializer.rb
@@ -1,6 +1,17 @@
 class CompanySerializer
   include FastJsonapi::ObjectSerializer
+
   attributes :slug, :name, :url, :brand
+  
+  attribute :brand do |object|
+    if object.brand.attached?
+      {
+        url: Rails.application.routes.url_helpers.rails_blob_url(
+          object.brand, disposition: "attachment", only_path: true
+        )
+      }
+    end
+  end
   
   has_many :position
 end
