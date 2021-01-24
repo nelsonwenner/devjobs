428c2050d32caabadc5aefe6f3ab5556249d34c7
wenner <nelsondiasdemedeiros@gmail.com>
2021-01-24 14:52:34 -0300

    test: ensure not be able to create a new company

--- spec/controllers/companies_controller_spec.rb
+++ spec/controllers/companies_controller_spec.rb
@@ -3,18 +3,33 @@ require 'rails_helper'
 RSpec.describe ::Api::V1::CompaniesController, type: :controller do
   describe "POST #create" do
     let(:valid_company) { attributes_for(:company) }
+    let(:invalid_company_name) { attributes_for(:company, name: nil) }
+    let(:invalid_company_url) { attributes_for(:company, url: nil) }
     let(:user) { create(:user) }
 
     before do
       session[:user_id] = user.id
     end
-
+    
     context 'With valid attributes' do
       it 'Should be able to create a new company' do
         expect{ 
           post :create, params: { company: valid_company }
         }.to change(Company, :count).by(1)
       end
+    end
+
+    context 'With invalid attributes' do
+      it {
+        expect{ 
+          post :create, params: { company: invalid_company_name }
+        }.to change(Company, :count).by(0)
+      }
+      it {
+        expect{ 
+          post :create, params: { company: invalid_company_url }
+        }.to change(Company, :count).by(0)
+      }
     end
   end
 end
