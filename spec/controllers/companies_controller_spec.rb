1525c12452b62678461f3329db09d6e39c401a3a
wenner <nelsondiasdemedeiros@gmail.com>
2021-01-24 16:01:12 -0300

    test: ensure be able to get resource with slug valid and reject-with slug invalid.

--- spec/controllers/companies_controller_spec.rb
+++ spec/controllers/companies_controller_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe ::Api::V1::CompaniesController, type: :controller do
     before do
       session[:user_id] = user.id
     end
-    
+
     context 'With valid attributes' do
       it 'Should be able to create a new company' do
         expect{ 
@@ -30,6 +30,28 @@ RSpec.describe ::Api::V1::CompaniesController, type: :controller do
           post :create, params: { company: invalid_company_url }
         }.to change(Company, :count).by(0)
       }
+    end
+  end
+
+  describe "GET #show" do
+    let(:user) { create(:user) }
+    let(:company) { create(:company, user: user) }
+    
+    before do
+      session[:user_id] = user.id
+    end
+
+    context 'With valid slug' 
+      it 'Should be able to get one resource' do
+        response = get :show, params: { slug: company.slug }
+        expect(assigns(:company)).to be_a(Company)
+        expect(response.status).to equal(200)
+    end
+    
+    context 'With invalid slug' 
+    it 'Should not be able to get one resource' do
+      response = get :show, params: { slug: 'test-slug' }
+      expect(response.status).to equal(404)
     end
   end
 end
