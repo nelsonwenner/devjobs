634d3322e97cb4b3facda1eaa87d5027e2b5a943
wenner <nelsondiasdemedeiros@gmail.com>
2021-01-24 13:48:52 -0300

    feat: add a method empty_brand

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,7 +1,9 @@
 class Company < ApplicationRecord
   belongs_to :user
   has_many :position, dependent: :destroy
-  
+
+  has_one_attached :brand
+    
   validates_presence_of :name, :url, on: :create
   
   before_create :add_slug
@@ -12,8 +14,16 @@ class Company < ApplicationRecord
   def add_slug
     self.slug = self.name.parameterize
   end
-
+  
   def empty_brand
-    self.brand = 'company_default_logo.png' if brand.blank?
+    self.brand.attach(
+      io: File.open(
+        Rails.root.join(
+          'app', 'assets', 'images', 'company_default_logo.png'
+        )
+      ),
+        filename: 'company_default_logo.png', 
+        content_type: 'image/png'
+    ) if brand.blank?
   end
 end
