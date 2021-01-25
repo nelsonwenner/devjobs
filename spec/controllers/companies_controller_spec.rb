743fc48ae889f26ba50dfdd06f8afce7e9c9a60b
wenner <nelsondiasdemedeiros@gmail.com>
2021-01-25 19:01:22 -0300

    test: ensure be able to update a company with attributes valid

--- spec/controllers/companies_controller_spec.rb
+++ spec/controllers/companies_controller_spec.rb
@@ -41,17 +41,44 @@ RSpec.describe ::Api::V1::CompaniesController, type: :controller do
       session[:user_id] = user.id
     end
     
-    context 'With valid id' 
+    context 'With valid id' do
       it 'Should be able to get one resource' do
         response = get :show, params: { id: company.id }
         expect(assigns(:company)).to be_a(Company)
         expect(response.status).to equal(200)
+      end
     end
     
-    context 'With invalid id' 
-    it 'Should not be able to get one resource' do
-      response = get :show, params: { id: -1 }
-      expect(response.status).to equal(404)
+    context 'With invalid id' do
+      it 'Should not be able to get one resource' do
+        response = get :show, params: { id: -1 }
+        expect(response.status).to equal(404)
+      end
+    end
+  end
+
+  describe "PUT #update" do
+    let(:user) { create(:user) }
+    let(:company) { create(:company, user: user) }
+
+    before do
+      session[:user_id] = user.id
+    end
+
+    context 'with valid attributes' do
+      let(:valid_company) { attributes_for(:company) }
+      
+      it {
+        response = put :update, params: { 
+          id: company.id, company: valid_company 
+        }
+        expect(
+          eval(response.body)[:data][:attributes][:name]
+        ).to eq(valid_company[:name])
+        expect(
+          eval(response.body)[:data][:attributes][:url]
+        ).to eq(valid_company[:url])
+      }
     end
   end
 end
