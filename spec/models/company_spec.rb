accf3788b20a1e1a0d8a8a33223def48dc3a012d
wenner <nelsondiasdemedeiros@gmail.com>
2021-01-25 19:49:35 -0300

    test: ensure be able to validation fields when doing an update

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -9,5 +9,7 @@ RSpec.describe Company, type: :model do
   describe 'Validations' do
     it { should validate_presence_of(:name).on(:create) }
     it { should validate_presence_of(:url).on(:create) }
+    it { should validate_presence_of(:name).on(:update) }
+    it { should validate_presence_of(:url).on(:update) }
   end
 end
