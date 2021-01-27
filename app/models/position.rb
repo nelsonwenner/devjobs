0044b41f8aa0b6382172be7a13f0f2c995477f8f
wenner <nelsondiasdemedeiros@gmail.com>
2021-01-26 23:08:57 -0300

    refactor: add relation with career

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,6 +1,9 @@
 class Position < ApplicationRecord
   belongs_to :company
+  belongs_to :career
   has_many :applicant, dependent: :destroy
 
-  validates_presence_of :name, :career, :contract, :remote, :country, :state, :city, :summary, :description, :publish, on: :create
+  validates_presence_of :name, :contract, :remote, :country, :state, 
+                        :city, :summary, :description, :publish, on: :create
+
 end
