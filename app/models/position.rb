56ff651f886f853cd94205ccc26bab234aff89c2
wenner <nelsondiasdemedeiros@gmail.com>
2021-01-28 14:59:09 -0300

    refactor: add a new field slug

--- app/models/position.rb
+++ app/models/position.rb
@@ -5,5 +5,12 @@ class Position < ApplicationRecord
 
   validates_presence_of :name, :contract, :country, :state, :city, 
                         :summary, :description, on: :create
+                        
+  before_create :add_slug
 
+  private
+
+  def add_slug
+    self.slug = self.name.parameterize
+  end
 end
