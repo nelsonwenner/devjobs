32dab4f448af1ba0fa440166cac3c4371736e6df
wenner <nelsondiasdemedeiros@gmail.com>
2021-01-29 19:24:20 -0300

    refactor: remove field slug

--- db/migrate/20210119142035_create_companies.rb
+++ db/migrate/20210119142035_create_companies.rb
@@ -2,7 +2,6 @@ class CreateCompanies < ActiveRecord::Migration[6.0]
   def change
     create_table :companies do |t|
       t.string :name
-      t.string :slug
       t.string :url
       t.references :user, null: false, foreign_key: true
 
