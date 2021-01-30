01450c1dafb75cda11c730ef045d91ac7a3e5914
wenner <nelsondiasdemedeiros@gmail.com>
2021-01-30 10:14:39 -0300

    feat: add curriculum field

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -1,6 +1,8 @@
 class Applicant < ApplicationRecord
   belongs_to :user
   belongs_to :position
+
+  has_one_attached :curriculum
   
-  validates_presence_of :name, :email, :phone, on: :create
+  validates_presence_of :name, :email, :phone
 end
