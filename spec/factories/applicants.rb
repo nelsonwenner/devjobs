de105b26f8df0a59795aa8285d55d6513c4812d8
wenner <nelsondiasdemedeiros@gmail.com>
2021-01-30 10:24:15 -0300

    feat: add applicants factory

--- spec/factories/applicants.rb
+++ spec/factories/applicants.rb
@@ -1,9 +1,13 @@
 FactoryBot.define do
   factory :applicant do
-    name { "MyString" }
-    email { "MyString" }
-    phone { "MyString" }
-    user { nil }
-    position { nil }
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    phone { '123456' }
+    position_id { nil }
+    curriculum { 
+      Rack::Test::UploadedFile.new(
+        Rails.root.join('app', 'assets', 'images', 'test', 'test_file_pdf.pdf')
+      )
+    }
   end
 end
