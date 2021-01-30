9891ce585638be995f9c616bb5c9dd5ad8628af8
wenner <nelsondiasdemedeiros@gmail.com>
2021-01-30 17:58:36 -0300

    test: ensure be able to sender a email

--- spec/mailers/reset_password_mailer_spec.rb
+++ spec/mailers/reset_password_mailer_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ResetPasswordMailer, type: :mailer do
-  token = 'xxzwxcLYz/D46K4RkJY0tchMZv0atSA9'
+  token = 'xxzwxcLYzD46K4RkJY0tchMZv0atSA9'
   email = 'test@email.com'
   default_from_email = 'noreply@company.com'
   let(:mail) { ResetPasswordMailer.call(email, token).deliver_now }
@@ -12,15 +12,19 @@ RSpec.describe ResetPasswordMailer, type: :mailer do
 
   describe "ResetPasswordMailer #call" do
     context 'when attributes are renders' do
+      it 'should be able to sender the email' do
+        expect(ActionMailer::Base.deliveries.count).to eq(1)
+      end
+      
       it 'should be able to render the subject' do
         expect(mail.subject).to eq('Password Reset')
       end
 
-      it 'should be able to receiver the email' do
+      it 'should be able to render the email to' do
         expect(mail.to).to eq(['test@email.com'])
       end
 
-      it 'should be able to sender the email' do
+      it 'should be able to render the email from' do
         expect(mail.from).to eq(['noreply@company.com'])
       end
     end
