7af44ccbcc5961e58aaa3491a4d8a5168b8e4904
wenner <nelsondiasdemedeiros@gmail.com>
2021-02-01 23:31:57 -0300

    chore: add environment reading if be development or test

--- config/application.rb
+++ config/application.rb
@@ -6,12 +6,14 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
+Dotenv::Railtie.load if Rails.env.test? || Rails.env.development?
 
 module Devjobs
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+
+    config.active_job.queue_adapter = :sidekiq
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
