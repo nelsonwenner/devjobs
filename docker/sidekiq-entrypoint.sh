f044b416a6ae7db64cd8041b91d31a7024a16ac1
wenner <nelsondiasdemedeiros@gmail.com>
2021-02-02 23:20:46 -0300

    chore: add entrypoint sidekiq

---
+++ docker/sidekiq-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd /usr/src/app
+
+if [ ! -f ".env" ]; then
+  cp .env.example .env
+fi
+
+if [ -f tmp/pids/server.pid ]; then
+  rm -f tmp/pids/server.pid
+fi
+
+dockerize -wait tcp://rails-app:3333 -timeout 2700s -wait-retry-interval 10s
+dockerize -wait tcp://postgres:5432 -timeout 2700s -wait-retry-interval 10s
+dockerize -wait tcp://redis:6379 -timeout 2700s -wait-retry-interval 10s
+
+(bundle check || bundle install)
+yarn install
+
+bundle exec sidekiq
