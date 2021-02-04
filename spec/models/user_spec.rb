2dba3e132d7715b44e2dbcb7153fdcea4d982cc8
wenner <nelsondiasdemedeiros@gmail.com>
2021-02-04 10:50:13 -0300

    test: ensure be able to generate a password token

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:user) { create(:user) }
+  
   describe 'Associations' do
     it { should have_one(:company) }
     it { should have_many(:applicant) }
@@ -10,5 +12,11 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:username).on(:create) }
     it { should validate_presence_of(:email).on(:create) }
     it { should validate_presence_of(:password).on(:create) }
+  end
+
+  describe '#generate_password_token' do
+    it 'Should be able to generate a password token' do
+      expect(user.generate_password_token!).to equal(true)
+    end
   end
 end
