f7843e723041fae14da26d11d59ce2848bb03d93
wenner <nelsondiasdemedeiros@gmail.com>
2021-02-06 22:59:19 -0300

    test: refactor some tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,24 +17,24 @@ RSpec.describe User, type: :model do
   describe '#generate_password_token' do
     it 'Should be able to generate a password token' do
       expect(user.generate_password_token!).to equal(true)
+      expect(user.reset_password_token).not_to be_nil
     end
   end
 
   describe '#password_token_valid' do
     it 'Should be able to verify password token valid' do
       user.generate_password_token!
-      user.reload()
       expect(user.password_token_valid?).to equal(true)
     end
   end
 
   describe '#reset_password' do
     it 'Should be able to reset password' do
-      new_password = '123456'
+      old_password = user.password_digest
       user.generate_password_token!
-      user.reload()
-      update_password = user.reset_password!(new_password)
+      update_password = user.reset_password!('123456')
       expect(update_password).to equal(true)
+      expect(user.password_digest).not_to equal(old_password)
     end
   end
 end
